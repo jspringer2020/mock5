c67b2ebb2c2b6ce5260944a96ec39078b7ed8e7e
Josh Springer <josh@symphono.com>
2014-11-20 16:21:52 -0500

    Created the Person and MarriageProposal classes. There is no implementation at this point.

---
+++ src/MarriageProposal.js
@@ -0,0 +1,55 @@
+var Person = require("Person");
+
+function MarriageProposal(proposingPerson, proposedToPerson) {
+  if (!(proposedToPerson instanceof Person)) {
+    throw new Error("The argument 'proposingPerson' does not conform to expected contract");
+  }
+
+  if (!(proposedToPerson instanceof Person)) {
+    throw new Error("The argument 'proposedToPerson' does not conform to expected contract");
+  }
+  
+  this._proposingPerson = proposingPerson;
+  this._proposedToPerson = proposedToPerson;
+}
+
+
+/**
+ * Validates the marriage proposal ensuring that it can in fact take place.
+ *
+ * @returns {(MarriageProposal.Validation|Array.)} A list of broken {MarriageProposal.ValidationCodes} that failed validation.
+ */
+MarriageProposal.prototype.validate = function validate() {
+  return [];
+};
+
+
+/**
+ * Creates all the actions of performing a marriage proposal including getting down on one knew,
+ * asking the question, receiving a response from the {otherPerson} and then returns the happiness level
+ * of the person proposing.
+ * 
+ * @param {Person} otherPerson The other person that is being proposed to.
+ * @returns {String} The new tax filing status
+ */
+Person.prototype.performMarriageProposal = function performMarriageProposal() {
+  return null;
+};
+
+
+var marriageValidation = {
+  10000: {
+    rule: function() {
+      return this._proposingPerson !== this._proposedToPerson;
+    }
+  },
+  10001: {
+    rule: function() {
+      return this._proposingPerson.getGender() !== this._proposedToPerson.getGender();
+    }
+  }
+};
+
+
+MarriageProposal.Validation = marriageValidation;
+module.exports = MarriageProposal;
---
+++ src/Person.js
@@ -0,0 +1,48 @@
+
+
+function Person(gender, name) {
+  if (!(gender instanceof Boolean)) {
+    throw new Error("The argument 'gender' does not conform to expected contract");
+  }
+  
+  this._gender = gender;
+  this._name = name;
+}
+
+
+/**
+ * Returns the gender of the person.
+ *
+ * @returns {Boolean} TRUE, for man; FALSE, for woman;
+ */
+Person.prototype.getGender = function getGender() {
+  return this._gender;
+};
+
+
+/**
+ * Returns the name of the person.
+ *
+ * @returns {String} The person's name
+ */
+Person.prototype.getName = function getName() {
+  return this._name;
+};
+
+
+/**
+ * Taking in a {Person} it determines the likelihood of a YES response for marriage. This is a long running method 
+ * because it examines of all the "history" between the two {Person}s. The result of this method is a positive 
+ * integer between 0 and 100 which describes how likely the response to the proposal will be YES.
+ * 
+ * @param {Person} proposal The marriage proposal
+ * @result {Integer} A percent of how likely the person is to respond YES to the proposal.
+ */
+Person.prototype.getMarriageProposalResponse = function getMarriageProposalResponse(/*otherPerson*/) {
+  return 0;
+};
+
+
+
+
+module.exports = Person;
