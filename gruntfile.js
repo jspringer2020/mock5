e002dd24ac6679ef5a1149232753d4edfeeb3908
Josh Springer <josh@symphono.com>
2014-11-21 14:26:12 -0500

    Added Istanbul code coverage as well as grunt step for printing coverage detail during grunt watch.

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,7 @@
-module.exports = function(grunt) {
+var lastGruntWatchTime = null,
+  watchRuns = 0;
+
+module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -6,14 +9,127 @@ module.exports = function(grunt) {
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*Spec.js']
     },
     watch: {
+      options: {
+        reload: true,
+        atBegin: true,
+        dateFormat: printWatchOutput
+      },
       files: ['<%= jshint.files %>'],
       reporter: 'Dot',
-      tasks: ['clear', 'lint', 'test']
+      tasks: ['clear', 'lint', 'test', 'coverage']
     },
     mochacli: {
-      options: {
-      },
+      options: {},
       all: ['./test/specs/**/*Spec.js']
+    },
+    mocha_istanbul: {
+      coverage: {
+        src: 'test', // a folder works nicely
+        options: {
+          root: './src',
+          quiet: true,
+          reportFormats: ['lcovonly'],
+          coverage: true,
+          check: {
+            lines: 50,
+            statements: 50
+          }
+        },
+        jenkins: {
+          src: 'test', // a folder works nicely
+          options: {
+            root: './src',
+            check: {
+              lines: 50,
+              statements: 50
+            },
+            reportFormats: ['lcovonly']
+          }
+        }
+      }
+    }
+  });
+
+
+  function printWatchOutput(time) {
+    var outputColor, bgMap, bgData, message, now;
+    var chanceJS = require("chance")();
+    require("colors");
+
+    now = new Date();
+    watchRuns++;
+    lastGruntWatchTime = lastGruntWatchTime || now;
+
+    bgMap = [
+      "bgRed|white",
+      "bgBlue|white",
+      "bgCyan|black",
+      "bgGreen|white",
+      "bgBlack|white",
+      "bgWhite|black",
+      "bgYellow|black",
+      "bgMagenta|black"
+    ];
+
+    bgData = bgMap[chanceJS.integer({
+      min: 0,
+      max: bgMap.length - 1
+    })];
+    
+    outputColor = {
+      bg: bgData.substring(0, bgData.indexOf("|") - 1),
+      fg: bgData.substring(bgData.indexOf("|"))
+    };
+
+    message = "Run #" + ("" + watchRuns) + " completed " +
+      (Math.floor((now.getTime() - lastGruntWatchTime.getTime()) / 1000)) + "secs ago in " +
+      time + "ms at " + now.toString();
+
+    grunt.log.writeln(message);
+    grunt.log.writeln(('Waiting for more changes...' [outputColor.bg])[outputColor.fg]);
+
+    lastGruntWatchTime = now;
+  }
+
+  grunt.event.on('coverage', function (lcovInfo, done) {
+    var lcovParse = require("lcov-parse");
+    require("colors");
+
+    lcovParse(lcovInfo, function (err, data) {
+      if (err) {
+        grunt.log.writeln("Error parsing lcov file".red);
+        done(false);
+        return;
+      }
+
+      printOutput(data[0]);
+    });
+
+    function printOutput(lcov) {
+      var functionsCovered, linesCovered;
+      functionsCovered = Math.round((lcov.functions.hit / lcov.functions.found), 4) * 100;
+      linesCovered = Math.round((lcov.lines.hit / lcov.lines.found), 4) * 100;
+
+      function getColor(percent) {
+        var result = "green";
+        if (percent < 50) {
+          result = "red";
+        } else if (result < 80) {
+          result = "yellow";
+        }
+
+        return result;
+      }
+
+      grunt.log.writeln("There were " + lcov.functions.hit + " of " + lcov.functions.found +
+        " functions covered for " + (functionsCovered.toString()[getColor(functionsCovered)]).bold +
+        "% coverage" [getColor(linesCovered)]);
+      grunt.log.writeln("There were " + lcov.lines.hit + " of " + lcov.lines.found +
+        " lines covered for " + (linesCovered.toString()[getColor(linesCovered)]).bold +
+        "% coverage" [getColor(linesCovered)]);
+      grunt.log.writeln("");
+      grunt.log.writeln("");
+      done(); // or done(false); in case of error
     }
   });
 
@@ -21,11 +137,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-clear');
   grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
 
   grunt.registerTask('test', ['mochacli']);
+  grunt.registerTask('coverage', ['mocha_istanbul:coverage']);
   grunt.registerTask('lint', ['jshint']);
-  grunt.registerTask('build', ['lint', 'test']);
+  grunt.registerTask('build', ['lint', 'test', 'coverage']);
 
   grunt.registerTask('default', ['build']);
-
 };
--- test/specs/PersonSpec.js
+++ test/specs/PersonSpec.js
@@ -1,150 +1,150 @@
-describe("Person", function() {
-  
+describe("Person", function () {
+
   var baseDirectory = "./../../";
   var proxyquire = require("proxyquire");
   var chance = require("chance")();
-//  var sinon = require("sinon");
-//  var should = require("should");
+  //  var sinon = require("sinon");
+  //  var should = require("should");
 
   var mocks = {};
-  
+
   function getModule() {
     var mod = proxyquire(baseDirectory + "src/Person", mocks);
     return mod;
   }
-  
-  describe("when creating a new person", function() {
+
+  describe("when creating a new person", function () {
     var Person;
-    
-    beforeEach(function() {
+
+    beforeEach(function () {
       Person = getModule();
     });
-    
-    describe("when the name is not valid", function() {
+
+    describe("when the name is not valid", function () {
       var construction;
 
-      describe("when the name is not supplied", function() {
-        beforeEach(function() {
-          construction = function() {
+      describe("when the name is not supplied", function () {
+        beforeEach(function () {
+          construction = function () {
             return new Person(true);
           };
         });
 
-        it("then an error is thrown", function() {
+        it("then an error is thrown", function () {
           construction.should.throwError();
         });
       });
 
-      describe("when the name is not a string", function() {
-        beforeEach(function() {
-          construction = function() {
+      describe("when the name is not a string", function () {
+        beforeEach(function () {
+          construction = function () {
             return new Person(true, chance.integer());
           };
         });
 
-        it("then an error is thrown", function() {
+        it("then an error is thrown", function () {
           construction.should.throwError();
         });
       });
     });
-    
-    describe("when the gender is not supplied", function() {
+
+    describe("when the gender is not supplied", function () {
       var construction;
 
-      beforeEach(function() {
-        construction = function() {
+      beforeEach(function () {
+        construction = function () {
           return new Person();
         };
       });
-      
-      it("then an error is thrown", function() {
+
+      it("then an error is thrown", function () {
         construction.should.throwError();
       });
     });
-    
-    describe("when all required parameters are passed", function() {
+
+    describe("when all required parameters are passed", function () {
       var result, name;
-      
-      describe("when its a male", function() {
-        beforeEach(function() {
+
+      describe("when its a male", function () {
+        beforeEach(function () {
           name = chance.string();
           result = new Person(true, name);
         });
-        
-        it("then the object is created", function() {
+
+        it("then the object is created", function () {
           result.should.be.ok
             .and.be.an.instanceOf(Person);
         });
 
-        it("then the gender is MALE", function() {
+        it("then the gender is MALE", function () {
           result.getGender().should.have.properties({
             _displayName: "Male",
             _isMale: true
           });
         });
 
-        it("then the name is set", function() {
+        it("then the name is set", function () {
           result.getName().should.equal(name);
         });
       });
 
-      describe("when its a female", function() {
-        beforeEach(function() {
+      describe("when its a female", function () {
+        beforeEach(function () {
           name = chance.string();
           result = new Person(false, name);
         });
-        
-        it("then the object is created", function() {
+
+        it("then the object is created", function () {
           result.should.be.ok
             .and.be.an.instanceOf(Person);
         });
 
-        it("then the gender is MALE", function() {
+        it("then the gender is MALE", function () {
           result.getGender().should.have.properties({
             _displayName: "Female",
             _isMale: false
           });
         });
 
-        it("then the name is set", function() {
+        it("then the name is set", function () {
           result.getName().should.equal(name);
         });
       });
     });
   });
-  
-  describe("when retrieving the gender of a person", function() {
+
+  describe("when retrieving the gender of a person", function () {
     var Person;
 
-    beforeEach(function() {
+    beforeEach(function () {
       Person = getModule();
     });
 
-    describe("when the gender is MALE", function() {
+    describe("when the gender is MALE", function () {
       var result;
-      
-      beforeEach(function() {
+
+      beforeEach(function () {
         var target = new Person(true, chance.string());
         result = target.getGender();
       });
-      
-      it("then the result is MALE", function() {
+
+      it("then the result is MALE", function () {
         result.should.have.properties({
           _isMale: true,
           _displayName: "Male"
         });
       });
     });
-    
-    describe("when the gender is FEMALE", function() {
+
+    describe("when the gender is FEMALE", function () {
       var result;
-      
-      beforeEach(function() {
+
+      beforeEach(function () {
         var target = new Person(false, chance.string());
         result = target.getGender();
       });
-      
-      it("then the result is FEMALE", function() {
+
+      it("then the result is FEMALE", function () {
         result.should.have.properties({
           _isMale: false,
           _displayName: "Female"
@@ -152,85 +152,85 @@ describe("Person", function() {
       });
     });
   });
-  
-  describe("when retrieving the name of a person", function() {
+
+  describe("when retrieving the name of a person", function () {
     var name, result;
-    
-    beforeEach(function() {
+
+    beforeEach(function () {
       name = chance.string();
-      
+
       var Person, target;
       Person = getModule();
       target = new Person(chance.bool(), name);
-      
+
       result = target.getName();
     });
-    
-    it("then the name matches", function() {
+
+    it("then the name matches", function () { 
       result.should.equal(name);
     });
   });
-  
-  describe("when determining the marriage proposal response", function() {
-    
-    describe("when no other person is supplied", function() {
-      
+
+  describe("when determining the marriage proposal response", function () {
+
+    describe("when no other person is supplied", function () {
+
       it("then an error is raised");
     });
-    
-    describe("when no history service provider is supplied", function() {
-      
+
+    describe("when no history service provider is supplied", function () {
+
       it("then an error is raised");
     });
-    
-    describe("when the marriage proposal isn't valid", function() {
-      
+
+    describe("when the marriage proposal isn't valid", function () {
+
       it("then an error is raised");
     });
-    
-    describe("when the marriage proposal is valid", function() {
-      
-      describe("when there is no personal history", function() {
-        
+
+    describe("when the marriage proposal is valid", function () {
+
+      describe("when there is no personal history", function () {
+
         it("then the result is 0");
-        
+
         it("then the history service provider was searched");
       });
-      
-      describe("when there is personal history", function() {
-        
-        describe("when the history results in a 100 response", function() {
-          
+
+      describe("when there is personal history", function () {
+
+        describe("when the history results in a 100 response", function () {
+
           it("then the result is 100");
-          
+
           it("then the history service provider was searched");
         });
-        
-        describe("when the history results in a 75 response", function() {
-          
+
+        describe("when the history results in a 75 response", function () {
+
           it("then the result is 75");
-          
+
           it("then the history service provider was searched");
         });
-        
-        describe("when the history results in a 35 response", function() {
-          
+
+        describe("when the history results in a 35 response", function () {
+
           it("then the result is 35");
-          
+
           it("then the history service provider was searched");
         });
-        
-        describe("when the history results in a 0 response", function() {
-          
-          describe("when all history is bad leading to a negative total", function() {
-            
+
+        describe("when the history results in a 0 response", function () {
+
+          describe("when all history is bad leading to a negative total", function () {
+
             it("then the result is 0");
 
             it("then the history service provider was searched");
           });
-          
-          describe("when history results in a 0 response", function() {
-            
+
+          describe("when history results in a 0 response", function () {
+
             it("then the result is 0");
 
             it("then the history service provider was searched");
