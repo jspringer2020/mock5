8918cd9d19df81764c14fb74e9756edcc1a7f169
Josh Springer <josh@symphono.com>
2014-11-21 10:57:06 -0500

    Added mocha unit test framework for unit tests.

--- gruntfile.js
+++ gruntfile.js
@@ -3,19 +3,29 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*Spec.js']
     },
     watch: {
       files: ['<%= jshint.files %>'],
-      tasks: ['clear', 'build']
+      tasks: ['clear', 'lint', 'test']
+    },
+    mochacli: {
+      options: {
+        "check-leaks": true, /* Check for global variable leaks*/
+        require: ['should'],
+        reporter: 'spec',
+        bail: true
+      },
+      all: ['./test/specs/**/*Spec.js']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-clear');
+  grunt.loadNpmTasks('grunt-mocha-cli');
 
-  grunt.registerTask('test', []);
+  grunt.registerTask('test', ['mochacli']);
   grunt.registerTask('lint', ['jshint']);
   grunt.registerTask('build', ['lint', 'test']);
 
