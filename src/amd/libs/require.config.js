3a2891206c692dad65e3fe6081be91544b138042
Josh Springer <josh@symphono.com>
2014-11-25 09:38:07 -0500

    Updated RequireJS paths so both the app and the specs can run using the same configuration.

--- src/amd/libs/require.config.js
+++ src/amd/libs/require.config.js
@@ -1,13 +1,13 @@
 /* globals requirejs */
 requirejs.config({
-  baseUrl: './src',
+  baseUrl: './src/',
   paths: {
     app: 'amd/main',
-    "jquery": "../../src/amd/libs/jquery-2.1.1.min",
-    "specRunner": "../SpecRunner",
-    "Squire": "../libs/Squire",
-    "should": "../libs/should.min",
-    "chance": "../libs/chance"
+    "jquery": "amd/libs/jquery-2.1.1.min",
+    "specRunner": "../test/SpecRunner",
+    "Squire": "../test/libs/Squire",
+    "should": "../test/libs/should.min",
+    "chance": "../test/libs/chance"
   },
   "shim": {
     "specRunner": {
